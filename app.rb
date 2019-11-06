2ee64ee5b320e5e81c0a67019a5aa87f75be34b0
KOlofinmoyin <kehindeolofinmoyin@hotmail.com>
2019-11-06 14:24:02 +0000

    first commit.

---
+++ app.rb
@@ -0,0 +1,16 @@
+require 'sinatra/base'
+require './lib/bookmark_base'
+
+class BookmarkManager < Sinatra::Base
+  get '/' do
+    erb :index
+  end
+
+
+  get '/bookmarks' do
+    @bookmarks = BookmarkBase.all
+    erb :bookmarks
+  end
+
+  run! if app_file == $0
+end
